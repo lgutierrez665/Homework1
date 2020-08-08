085353d97a2ef194ffac5e73147c4e07321407de
David C Hernandez, MSW, MBA <dhernandez@msjc.edu>
2020-08-08 13:45:22 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Author: David C Hernandez, MSW, MBA
+// GitHub:
+// August 08 2020
+// CSIS 113A
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
