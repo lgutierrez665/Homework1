03fec042c68b1f2552800d2932c5ea589186f462
David C Hernandez, MSW, MBA <dhernandez@msjc.edu>
2020-08-08 14:02:12 -0700

    changed code to utilize namespace std and added github address.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 // Author: David C Hernandez, MSW, MBA
-// GitHub:
-// August 08 2020
+// GitHub: https://github.com/dhernandez-msjc/csis_113a_output_and_literals.git
+// August 08, 2020
 // CSIS 113A
 #include <iostream>
+
 using namespace std;
 
 int main() {
