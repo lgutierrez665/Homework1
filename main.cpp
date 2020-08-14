bf19cf9ddbef7b54e1fa7cf672f12232431a5d6e
David C Hernandez, MSW, MBA <dhernandez@msjc.edu>
2020-08-13 20:55:00 -0700

    added README.MD

--- main.cpp
+++ main.cpp
@@ -7,6 +7,17 @@
 using namespace std;
 
 int main() {
+  // displays the string literal "Hello world!" to the console.
   cout << "Hello world!" << endl;
-  return 0;
+
+  // display an example of a receipt with only literals.
+  cout << "===================================" << endl;
+  cout << "                       Aug 08, 2020" << endl;
+  cout << "===================================" << endl;
+  cout << "Customer: Barry Allen" << endl;
+  cout << "Large pizzas: " << 2 << endl;
+  cout << "\n";
+  cout << "                   ----------------" << endl;
+  cout << "                   $          " << 22.47 << endl;
+  cout << "===================================" << endl;
 }
