2915cbf14d7d1958ab02355e9b7877b404f326cd
David C Hernandez, MSW, MBA <dhernandez@msjc.edu>
2020-08-14 00:36:37 -0700

    added first activity.

---
+++ activity_02.cpp
@@ -0,0 +1,11 @@
+// Author: David C Hernandez, MSW, MBA
+// GitHub: https:
+// August 14 2020
+// CSIS 113A
+// TODO: start by adding the include statement
+
+// TODO: add in using statement(s) for printing to the console
+
+// TODO: create main function
+
+// TODO: add commands to display your name to the console
