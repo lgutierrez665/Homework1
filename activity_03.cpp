be76adefb7755449fe24c3c10cc2ba1fcdbd5966
David C Hernandez, MSW, MBA <dhernandez@msjc.edu>
2020-08-14 01:19:55 -0700

    updated activity 03 content.

--- activity_03.cpp
+++ activity_03.cpp
@@ -5,6 +5,23 @@ using std::endl;
 
 int main()
 {
+    // in the last exercise, we utilized a string literal as our message.
+    // string literals display literally what is present, although, some characters are
+    // read differently, such as ", so we use the escape character before displaying "\"
+    cout << "Say \"hello\" to my little friend." << endl;
+
+    // notice that spacing within a string literal can allow us to create the appearance of
+    // formatting.
+    cout << "***************" << endl;
+    cout << "*             *" << endl;
+    cout << "*             *" << endl;
+    cout << "*             *" << endl;
+    cout << "*             *" << endl;
+    cout << "***************" << endl;
+
+    // lets add some spacing between this picture and the next set of outputs
+    cout << "\n\n" << endl;
+
     // display an example of a receipt with only literals.
     cout << "===================================" << endl;
     cout << "                       Aug 08, 2020" << endl;
---
+++ activity_04.cpp
@@ -0,0 +1,13 @@
+// Author: David C Hernandez, MSW, MBA
+// GitHub: https:
+// August 14 2020
+// CSIS 113A
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    // TODO: re-create the receipt from activity 03, adding an item
+}
