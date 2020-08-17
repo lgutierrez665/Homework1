f80b3914a9096f112a56b6df63034db05390483e
David C Hernandez, MSW, MBA <dhernandez@msjc.edu>
2020-08-17 03:08:55 -0700

    updated assigment

--- challenge_01.cpp
+++ challenge_01.cpp
@@ -1,1 +1,20 @@
-// TODO: attempt the challenge to create a receipt using only literals (different types)
+/* TODO: attempt the challenge to create a receipt using only literals (different types)
+ * The final output should look similar to the following:
+ *
+ * ==================================
+ *                       Aug 17, 2020
+ * ==================================
+ * Customer: Bob Kent
+ *
+ * Pizzas  : 2
+ * Price   : $ 4.25
+ *
+ *                    SubTotal: $8.50
+ *                         Tax: $1.00
+ *                    ---------------
+ *                    $          9.50
+ *
+ *             Thank You!
+ * ==================================
+ *
+ * */
